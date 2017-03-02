1d81ad82b2a5ca2a8c9c6cc3b646e71e2846e6b7
Robert Reilly <robertjreilly@gmail.com>
2017-03-02 17:23:07 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# app_jenkins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: app_jenkins
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
